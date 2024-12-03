9ea20053387c5fcc5eea10bbe9f540594c4955b2
DiogoPinto13 <pinto.diogo2002@gmail.com>
2024-12-03 08:41:27 +0000

    added knowledge distillation logic

--- train.py
+++ train.py
@@ -14,10 +14,6 @@ from PIL import Image
 
 input_shape = (30, 30, 3)
 regularizer = tf.keras.regularizers.l2(3)#0.0001)
-optimizer = tf.keras.optimizers.Adam(
-    learning_rate=0.00001,
-    amsgrad=True
-)
 
 class Distiller(keras.Model):
     def __init__(self, student, teacher):
@@ -70,6 +66,34 @@ class Distiller(keras.Model):
     def call(self, x):
         return self.student(x)
     
+
+class DataGenerator(Sequence):
+    def __init__(self, dataframe, root, batch_size=32, shuffle=True):
+        self.df = dataframe.sample(frac=1).reset_index(drop=True) if shuffle else dataframe
+        self.root = root
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+    def __len__(self):
+        return int(np.ceil(len(self.df) / self.batch_size))
+
+    def __getitem__(self, idx):
+        batch_df = self.df[idx * self.batch_size:(idx + 1) * self.batch_size]
+        X = []
+        y = []
+        for _, row in batch_df.iterrows():
+            image_path = f'{self.root}/{row.image_name}'
+            image = cv2.imread(image_path)
+            resized_image = cv2.resize(image, (30, 30))
+            normalized_image = resized_image / 255.0
+            X.append(normalized_image)
+            y.append(int(row.number))
+        return np.array(X), np.array(y)
+
+    def on_epoch_end(self):
+        if self.shuffle:
+            self.df = self.df.sample(frac=1).reset_index(drop=True)
+
 def predictImage(model, image_path):
     image = cv2.imread(image_path)
     resized_image = cv2.resize(image, (30, 30))
@@ -79,78 +103,6 @@ def predictImage(model, image_path):
     prediction = model.predict(normalized_image)
     print(prediction)
     print(np.argmax(prediction))
-
-# model = Sequential()
-# model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=input_shape))
-# model.add(Conv2D(filters=64, kernel_size=(5,5), activation='relu'))
-# model.add(MaxPool2D(pool_size=(2, 2)))
-# model.add(Dropout(rate=0.15))
-# model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
-# model.add(Conv2D(filters=256, kernel_size=(3, 3), activation='relu'))
-# model.add(MaxPool2D(pool_size=(2, 2)))
-# model.add(Dropout(rate=0.20))
-# model.add(Flatten())
-# model.add(Dense(512, activation='relu'))
-# model.add(Dropout(rate=0.25))
-# model.add(Dense(43, activation='softmax'))
-
-model = tf.keras.models.load_model("traffic_classifier.h5")
-#model.compile(optimizer='adam', loss='mean_squared_error')
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-              metrics=['accuracy'])
-
-model.summary()
-
-previousOutputLayer = model.layers[len(model.layers) - 1].get_weights()
-
-#freeze backbone
-model._layers.pop()
-for layer in model.layers:
-    layer.trainable = False
-
-#add extra class
-model.add(Dense(45, activation='softmax', kernel_regularizer=regularizer))
-model.summary()
-
-#add the weights of the previous layer
-currentOutputLayer = model.layers[len(model.layers) - 1].get_weights()
-
-# print("WEIGHTS BEFORE")
-# print(previousOutputLayer[0])
-# print("BISASES BEFORE")
-# print(previousOutputLayer[1])
-
-#copy the parameters from the previous output layer to the new
-for i in range(len(previousOutputLayer[0])):
-    for j in range(len(previousOutputLayer[0][i])):
-        currentOutputLayer[0][i][j] = previousOutputLayer[0][i][j]
-
-for i in range(len(previousOutputLayer[1])):
-    currentOutputLayer[1][i] = previousOutputLayer[1][i]
-
-# print("WEIGHTS AFTER")
-# print(currentOutputLayer[0])
-# print("BISASES AFTER")
-# print(currentOutputLayer[1])
-
-#update the actual output layer
-print("Updating the parameters...")
-#print(model.layers[len(model.layers) - 1].get_weights())
-model.layers[len(model.layers) - 1].set_weights(currentOutputLayer)
-
-#print("Checking if the parameters are actually updated...")
-#print(model.layers[len(model.layers) - 1].get_weights())
-
-model.compile(optimizer=optimizer,
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-              metrics=['accuracy'])
-
-model.summary()
-
-previousModel = keras.models.clone_model(model)
-print("cloning...")                                                        
-previousModel.summary()
 
 def readImages(path):
     # Specify the parent folder path
@@ -185,113 +137,196 @@ def readImages(path):
     df['number'] = df['number'].replace(class_mapping)
     return df
 
-class DataGenerator(Sequence):
-    def __init__(self, dataframe, root, batch_size=32, shuffle=True):
-        self.df = dataframe.sample(frac=1).reset_index(drop=True) if shuffle else dataframe
-        self.root = root
-        self.batch_size = batch_size
-        self.shuffle = shuffle
+# model = Sequential()
+# model.add(Conv2D(filters=32, kernel_size=(5,5), activation='relu', input_shape=input_shape))
+# model.add(Conv2D(filters=64, kernel_size=(5,5), activation='relu'))
+# model.add(MaxPool2D(pool_size=(2, 2)))
+# model.add(Dropout(rate=0.15))
+# model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu'))
+# model.add(Conv2D(filters=256, kernel_size=(3, 3), activation='relu'))
+# model.add(MaxPool2D(pool_size=(2, 2)))
+# model.add(Dropout(rate=0.20))
+# model.add(Flatten())
+# model.add(Dense(512, activation='relu'))
+# model.add(Dropout(rate=0.25))
+# model.add(Dense(43, activation='softmax'))
 
-    def __len__(self):
-        return int(np.ceil(len(self.df) / self.batch_size))
+originalAccuracyList = list()
+newAccuracyList = list()
 
-    def __getitem__(self, idx):
-        batch_df = self.df[idx * self.batch_size:(idx + 1) * self.batch_size]
-        X = []
-        y = []
-        for _, row in batch_df.iterrows():
-            image_path = f'{self.root}/{row.image_name}'
-            image = cv2.imread(image_path)
-            resized_image = cv2.resize(image, (30, 30))
-            normalized_image = resized_image / 255.0
-            X.append(normalized_image)
-            y.append(int(row.number))
-        return np.array(X), np.array(y)
+def train():
+    model = tf.keras.models.load_model("traffic_classifier.h5")
+    #model.compile(optimizer='adam', loss='mean_squared_error')
+    model.compile(optimizer=tf.keras.optimizers.Adam(
+                    learning_rate=0.00001,
+                    amsgrad=True),
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+                metrics=['accuracy'])
 
-    def on_epoch_end(self):
-        if self.shuffle:
-            self.df = self.df.sample(frac=1).reset_index(drop=True)
+    #model.summary()
 
+    previousOutputLayer = model.layers[len(model.layers) - 1].get_weights()
 
-dfTrain = readImages("train")
-dfTest = readImages("test")
-dfVal = readImages("val")
+    #freeze backbone
+    model._layers.pop()
+    for layer in model.layers:
+        layer.trainable = False
 
-trainGen = DataGenerator(dfTrain, "mixedDataset/train", batch_size=8, shuffle=True)
-testGen = DataGenerator(dfTest, "mixedDataset/test", batch_size=8, shuffle=True)
-valGen = DataGenerator(dfVal, "mixedDataset/val", batch_size=8, shuffle=True)
+    #add extra class
+    model.add(Dense(45, activation='softmax', kernel_regularizer=regularizer))
+    #model.summary()
 
+    #add the weights of the previous layer
+    currentOutputLayer = model.layers[len(model.layers) - 1].get_weights()
 
-history = model.fit(
-    trainGen,
-    epochs=13, #13,
-    validation_data=valGen
-)
+    # print("WEIGHTS BEFORE")
+    # print(previousOutputLayer[0])
+    # print("BISASES BEFORE")
+    # print(previousOutputLayer[1])
 
-testLoss, testAcc = model.evaluate(testGen)
+    #copy the parameters from the previous output layer to the new
+    for i in range(len(previousOutputLayer[0])):
+        for j in range(len(previousOutputLayer[0][i])):
+            currentOutputLayer[0][i][j] = previousOutputLayer[0][i][j]
 
-print("Test loss = " + str(testLoss))
-print("Accuracy = " + str(testAcc))
+    for i in range(len(previousOutputLayer[1])):
+        currentOutputLayer[1][i] = previousOutputLayer[1][i]
 
-#fine tuning step
-for layer in model.layers:
-    layer.trainable = True
+    # print("WEIGHTS AFTER")
+    # print(currentOutputLayer[0])
+    # print("BISASES AFTER")
+    # print(currentOutputLayer[1])
 
+    #update the actual output layer
+    print("Updating the parameters...")
+    #print(model.layers[len(model.layers) - 1].get_weights())
+    model.layers[len(model.layers) - 1].set_weights(currentOutputLayer)
 
-print("Fine tune process")
-history = model.fit(
-    trainGen,
-    epochs=5,
-    validation_data=valGen
-)
+    #print("Checking if the parameters are actually updated...")
+    #print(model.layers[len(model.layers) - 1].get_weights())
 
-testLoss, testAcc = model.evaluate(testGen)
-
-print("Test loss = " + str(testLoss))
-print("Accuracy = " + str(testAcc))
-
-distiller = Distiller(student=model, teacher=previousModel)
-distiller.compile(
-    optimizer=tf.keras.optimizers.Adam(
+    model.compile(optimizer=tf.keras.optimizers.Adam(
         learning_rate=0.00001,
         amsgrad=True),
-    metrics=['accuracy'],
-    student_loss_fn=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-    distillation_loss_fn=tf.keras.losses.KLDivergence(),
-    alpha=0.001,#0.0001,
-    temperature=10#0.01
-)
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+        metrics=['accuracy'])
+
+    #model.summary()
+
+    previousModel = keras.models.clone_model(model)
+    print("cloning...")                                                        
+    #previousModel.summary()
+
+    
+
+    dfTrain = readImages("train")
+    dfTest = readImages("test")
+    dfVal = readImages("val")
+
+    trainGen = DataGenerator(dfTrain, "mixedDataset/train", batch_size=8, shuffle=True)
+    testGen = DataGenerator(dfTest, "mixedDataset/test", batch_size=8, shuffle=True)
+    valGen = DataGenerator(dfVal, "mixedDataset/val", batch_size=8, shuffle=True)
 
 
-history = distiller.fit(trainGen, epochs=1, validation_data=valGen)
-distiller.evaluate(testGen)
+    history = model.fit(
+        trainGen,
+        epochs=13, #13,
+        validation_data=valGen
+    )
+
+    testLoss, testAcc = model.evaluate(testGen)
+
+    print("Test loss = " + str(testLoss))
+    print("Accuracy = " + str(testAcc))
+
+    #fine tuning step
+    for layer in model.layers:
+        layer.trainable = True
 
 
-#evaluate using the original dataset
-from sklearn.metrics import accuracy_score
+    print("Fine tune process")
+    history = model.fit(
+        trainGen,
+        epochs=5,
+        validation_data=valGen
+    )
 
-# Importing the test dataset
-y_test = pd.read_csv('Test.csv')
+    testLoss, testAcc = model.evaluate(testGen)
 
-labels = y_test["ClassId"].values
-imgs = y_test["Path"].values
+    print("Test loss = " + str(testLoss))
+    print("Accuracy = " + str(testAcc))
 
-data=[]
+    distiller = Distiller(student=model, teacher=previousModel)
+    distiller.compile(
+        optimizer=tf.keras.optimizers.Adam(
+            learning_rate=0.00001,
+            amsgrad=True),
+        metrics=['accuracy'],
+        student_loss_fn=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+        distillation_loss_fn=tf.keras.losses.KLDivergence(),
+        alpha=0.001,#0.0001,
+        temperature=10#0.01
+    )
 
-# Retreiving the images
-with tf.device('/GPU:0'):
-    for img in imgs:
-        image = Image.open(img)
-        image = image.resize([30, 30])
-        data.append(np.array(image))
+    history = distiller.fit(trainGen, epochs=1, validation_data=valGen)
+    newDatasetLoss, newDatasetAccuracy = distiller.evaluate(testGen)
 
-X_test=np.array(data)
-with tf.device('/GPU:0'):
-    pred = np.argmax(distiller.predict(X_test), axis=-1)
+    newAccuracyList.append(newDatasetAccuracy)
+    print("New dataset accuracy: " + str(newDatasetAccuracy))
 
-#Accuracy with the test data
-from sklearn.metrics import accuracy_score
-print(accuracy_score(labels, pred))
+    #evaluate using the original dataset
+    from sklearn.metrics import accuracy_score
 
-distiller.save('final_distiller.keras')
-#tf.keras.models.save_model(distiller, "distiller_2.h5")
+    # Importing the test dataset
+    y_test = pd.read_csv('Test.csv')
+
+    labels = y_test["ClassId"].values
+    imgs = y_test["Path"].values
+
+    data=[]
+
+    # Retreiving the images
+    with tf.device('/GPU:0'):
+        for img in imgs:
+            image = Image.open(img)
+            image = image.resize([30, 30])
+            data.append(np.array(image))
+
+    X_test=np.array(data)
+    with tf.device('/GPU:0'):
+        pred = np.argmax(distiller.predict(X_test), axis=-1)
+
+    #Accuracy with the test data
+    from sklearn.metrics import accuracy_score
+    originalAccuracy = accuracy_score(labels, pred)
+    originalAccuracyList.append(originalAccuracy)
+
+    print("Original dataset accuracy: " + str(originalAccuracy))
+
+    #distiller.save('final_distiller.keras')
+    #tf.keras.models.save_model(distiller, "distiller_2.h5")
+    keras.backend.clear_session()
+    distiller = None
+    previousModel = None
+    model = None
+
+def main():
+    for i in range(30):
+        train()
+    print("####################")
+    print("FINAL RESULTS")
+    print("####################")
+
+    print("Lists:")
+    print(originalAccuracyList)
+    print(newAccuracyList)
+    
+    print("Original dataset accuracy " + str(np.mean(originalAccuracyList)) + " with std of " + str(np.std(originalAccuracyList)))
+    print("New dataset accuracy " + str(np.mean(newAccuracyList)) + " with std of " + str(np.std(newAccuracyList)))
+
+
+
+
+if __name__ == "__main__":
+    main()
+
